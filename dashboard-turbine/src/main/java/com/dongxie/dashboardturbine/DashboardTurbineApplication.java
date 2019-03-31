2e0a3994e3c61437d2fbbc62ea6946c900ff027e
guan.xb <guan.xiangbiao@iwhalecloud.com>
2019-03-31 22:53:55 +0800

    Fix: 删除多余的依赖

--- dashboard-turbine/src/main/java/com/dongxie/dashboardturbine/DashboardTurbineApplication.java
+++ dashboard-turbine/src/main/java/com/dongxie/dashboardturbine/DashboardTurbineApplication.java
@@ -1,6 +1,5 @@
 package com.dongxie.dashboardturbine;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -10,6 +9,8 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
@@ -17,30 +18,28 @@ import org.springframework.context.annotation.Bean;
 @EnableTurbine
 public class DashboardTurbineApplication {
 
-	/**
-	 * http://localhost:8888/turbine.stream
-	 */
+    /**
+     * http://localhost:8888/turbine.stream
+     */
 
-	public static void main(String[] args) {
-		SpringApplication.run(DashboardTurbineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardTurbineApplication.class, args);
+    }
 
-	/**
-	 * 熔断追踪仪表盘 ： http://localhost:8883/hystrix
-	 * 这个地方日坑，用了springboot2.0和springcloud  Finchley版本
-	 * 必须加上这个servlet才可以  而且访问地址必须是下面这种形式
-	 * http://localhost:8883/actuator/hystrix.stream
-	 * @return
-	 */
-	@Bean
-	public ServletRegistrationBean getServlet(){
-		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-		registrationBean.setLoadOnStartup(1);
-		registrationBean.addUrlMappings("/actuator/hystrix.stream");
-		registrationBean.setName("HystrixMetricsStreamServletTurbine");
-		return registrationBean;
-	}
-
+    /**
+     * 熔断追踪仪表盘 ： http://localhost:8883/hystrix 这个地方日坑，用了springboot2.0和springcloud Finchley版本 必须加上这个servlet才可以
+     * 而且访问地址必须是下面这种形式 http://localhost:8883/actuator/hystrix.stream
+     * 
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServletTurbine");
+        return registrationBean;
+    }
 
 }
